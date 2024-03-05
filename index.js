2cd96a69282d0fb2bdf7ac69195262487c7aeea7
Deepesh Kumar <121308316+deepeshkumar06@users.noreply.github.com>
2024-03-05 11:21:16 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+// // var a = 1;
+// // while(a < 10){
+// //    console.log(a);
+// //     a++;
+// // }
+
+
+// // if(true){
+// //     let a = 5;
+// // }
+
+// const a = 4;
+// a = 3;
+
+// console.log(a);
+
+// // alert("hi");
+
+
+let a = [1,2,3,4,45,'Hello'];
+a.map((i,index) => {console.log(i +" with index "+index)});
